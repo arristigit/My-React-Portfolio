f81c0698f72e2a4d54338b3bea3842ea67aa3179
arristigit <arristimedia@gmail.com>
2021-11-06 15:58:59 +0530

    Hero/front section developed

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,19 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello there! <br />
+        I am VARINDER KUMAR
+      </SectionTitle>
+      <SectionText>
+        Full stack developer <br /> Regular working on Software Development Life Cycle (SDLC)
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Know More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
